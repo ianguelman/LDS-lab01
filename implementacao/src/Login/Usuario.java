2c2c408a51c6ea7d5a5ad548bf007b8a8f4ac3d6
Ian Guelman <ianguelman@gmail.com>
2021-03-16 10:11:04 -0300

    Metodos login

--- implementacao/src/Login/Usuario.java
+++ implementacao/src/Login/Usuario.java
@@ -3,5 +3,13 @@ package Login;
 public class Usuario {
 	private String login;
 	private String senha;
-
+	
+	public Usuario(String login, String senha) {
+		this.login = login;
+		this.senha = senha;
+	}
+	
+	public boolean logar(String login, String senha) {
+		return login.equals(this.login) && senha.equals(this.senha);
+	}
 }
