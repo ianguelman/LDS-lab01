778826e48c0486f773ee22c8b016d603185da58c
Matthew Nascimento <matthewromie@hotmail.com>
2021-03-16 10:54:55 -0300

    SistemaCobranca

--- implementacao/src/SistemaFinanceiro/Secretaria.java
+++ implementacao/src/SistemaFinanceiro/Secretaria.java
@@ -1,5 +1,10 @@
 package SistemaFinanceiro;
 
-public class Secretaria {
-
+public class Secretaria implements SistemaCobranca {
+	public void notificarSistemaCobranca() {
+		System.out.println("Notificação cobrança!");
+	}
+	public void cobrarAluno() {
+		System.out.println("Notificar aluno!");
+	}
 }
--- implementacao/src/SistemaFinanceiro/SistemaCobranca.java
+++ implementacao/src/SistemaFinanceiro/SistemaCobranca.java
@@ -1,5 +1,5 @@
 package SistemaFinanceiro;
 
 public interface SistemaCobranca {
-
+	public void cobrarAluno();
 }
