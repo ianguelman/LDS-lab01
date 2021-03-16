74a6c11231a27bc5ddb52170b6e604ba0987342f
Ian Guelman <ianguelman@gmail.com>
2021-03-16 10:41:35 -0300

    menus TODO

---
+++ implementacao/src/Ui/App.java
@@ -0,0 +1,22 @@
+package Ui;
+
+public class App {
+	
+	public void menuAluno() {
+		// TODO
+	}
+	
+	public void menuProfessor() {
+		// TODO
+	}
+	
+	public void menuSecretaria() {
+		// TODO
+	}
+
+	public static void main(String[] args) {
+		// TODO
+
+	}
+
+}
