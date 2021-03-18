df7df8416b895bada2151aa956889f2b6e60c158
Matthew Nascimento <matthewromie@hotmail.com>
2021-03-18 15:39:17 -0300

    Secretaria

--- implementacao/src/SistemaFinanceiro/Secretaria.java
+++ implementacao/src/SistemaFinanceiro/Secretaria.java
@@ -1,5 +1,7 @@
 package SistemaFinanceiro;
 
+import java.util.Scanner;
+
 import Login.Usuario;
 
 public class Secretaria extends Usuario implements SistemaCobranca {
@@ -13,5 +15,21 @@ public class Secretaria extends Usuario implements SistemaCobranca {
 	}
 	public void cobrarAluno() {
 		System.out.println("Notificar aluno!");
+	}
+	
+	public void criarAluno() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o nome do aluno: ");
+		String nome = sc.next();
+		System.out.print("Digite a matricula do aluno: ");
+		Long matricula = sc.nextLong();
+		new Aluno(nome, matricula);
+	}
+	
+	public void criarProfessor() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Digite o nome do professor: ");
+		String nome = sc.next();
+		new Professor(nome);
 	}
 }
